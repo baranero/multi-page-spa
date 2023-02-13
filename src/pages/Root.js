bb14e5e69b959ed30b84cd751ddf7fa16aa86c6d
baranero <jakub.baran2@gmail.com>
2023-02-13 22:32:19 +0100

    add styles to nav and root

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,11 +1,14 @@
 import { Fragment } from "react"
 import { Outlet } from "react-router"
 import MainNavigation from "../components/MainNavigation"
+import classes from './Root.module.css'
 
 function Root() {
   return <Fragment>
     <MainNavigation/>
+    <main className={classes.content}>
     <Outlet/>
+    </main>
   </Fragment>
 }
 
