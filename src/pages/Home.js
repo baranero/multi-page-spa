9f53fa5cd72f73672ac0c257783fd25363a9ff46
baranero <jakub.baran2@gmail.com>
2023-02-13 23:02:48 +0100

    useNavigate

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,21 @@
 import { Fragment } from "react"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 function HomePage() {
+  const navigate = useNavigate()
+
+  function navigateHandler() {
+    navigate('/products')
+  }
+
   return <Fragment>
     <h1>My Home Page</h1>
     <p>Go to <Link to="/products">Products</Link></p>
+    <p>
+      <button onClick={navigateHandler}>
+        Navigate
+      </button>
+    </p>
   </Fragment>
 }
 
